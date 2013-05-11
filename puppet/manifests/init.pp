81faaa13db22186206e0411d607e46123d9176b6
Daniel Diaz <diaz.carrete@facebook.com>
2013-05-11 02:20:04 +0200

    now with svn 1.7.x

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -9,11 +9,6 @@ class requirements {
 
 File { owner => 0, group => 0, mode => 0644 }
 
-file { '/etc/motd':
-  content => "Welcome to your Vagrant-built virtual machine!
-              Managed by Puppet.\n"
-}
-
 class installrvm {
   include rvm
   rvm::system_user { vagrant: ; }
@@ -53,3 +48,32 @@ package {
       ensure => installed,
   }
 
+package {
+  'python-software-properties':
+      ensure => installed,
+}
+
+exec { 'add-wan-repo':
+  command => '/usr/bin/sudo /usr/bin/add-apt-repository "deb http://opensource.wandisco.com/ubuntu lucid svn17"',
+  unless => '/bin/grep "lucid svn17" /etc/apt/sources.list',
+  require => Package['python-software-properties'],
+}
+
+exec { 'add-wan-repo-key':
+  command => 'sudo /usr/bin/wget -q http://opensource.wandisco.com/wandisco-debian.gpg -O- | sudo apt-key add -',
+  path  => "/usr/bin/",
+  refreshonly => true,
+}
+
+exec { 'add-wan-repo-update':
+  command => '/usr/bin/apt-get -y update',
+  refreshonly => true,
+}
+
+Exec['add-wan-repo'] ~> Exec['add-wan-repo-key'] ~> Exec['add-wan-repo-update'] # ~ is for notifications
+
+package {
+  'subversion':
+      ensure => 'installed',
+      require => Exec['add-wan-repo-update'] # requires on exec always seem to work, even if the execs aren't executed...
+}
