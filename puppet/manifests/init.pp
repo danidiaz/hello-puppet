9c4dd5ee4c27ae12d92c0564959776989adad246
Daniel Diaz <diaz.carrete@facebook.com>
2013-05-12 10:11:52 +0200

    added oracle java

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -57,7 +57,8 @@ package {
 }
 
 exec { 'add-wan-repo':
-  command => '/usr/bin/sudo /usr/bin/add-apt-repository "deb http://opensource.wandisco.com/ubuntu lucid svn17"',
+  command => 'sudo add-apt-repository "deb http://opensource.wandisco.com/ubuntu lucid svn17"',
+  path  => "/usr/bin/",
   unless => '/bin/grep "lucid svn17" /etc/apt/sources.list',
   require => Package['python-software-properties'],
 }
@@ -80,3 +81,38 @@ package {
       ensure => 'installed',
       require => Exec['add-wan-repo-update'] # requires on exec always seem to work, even if the execs aren't executed...
 }
+
+# oracle jdk
+# adapted from here: http://architects.dzone.com/articles/puppet-installing-oracle-java
+exec { 'add-java-repo':
+  command => 'sudo add-apt-repository ppa:webupd8team/java',
+  path  => "/usr/bin/",
+  creates => '/etc/apt/sources.list.d/webupd8team-java-lucid.list',
+  require => Package['python-software-properties'],
+  notify => Exec["add-java-repo-update","set-java-licence-selected","set-java-licence-seen"],
+}
+
+exec { 
+   'add-java-repo-update':
+      command => '/usr/bin/apt-get -y update',
+      refreshonly => true;
+
+   'set-java-licence-selected':
+     command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/sudo /usr/bin/debconf-set-selections',
+     refreshonly => true; 
+
+   'set-java-licence-seen':
+     command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/sudo /usr/bin/debconf-set-selections',
+     refreshonly => true; 
+}
+
+package { 'oracle-java6-installer':
+   ensure => 'installed',
+   require => Exec["add-java-repo-update","set-java-licence-selected","set-java-licence-seen"],
+}
+
+package { 'oracle-java6-set-default':
+   ensure => 'installed',
+   require => Package['oracle-java6-installer'],
+}
+
